bcfd90ddbf9d8c987fbd8c7aa394473247ec69e7
anthony <tony20402014@gmail.com>
2023-08-19 20:54:41 -0600

    dist tres mil cuatrosientos

--- src/components/componentesCitas/citasLocales.vue
+++ src/components/componentesCitas/citasLocales.vue
@@ -22,7 +22,7 @@ const showButton = ref(true);
 const fechaCita = ref('');
 const motivo = ref('');
 
-const regiss = ref(true);
+const regiss = ref(false);
 
 function regis(value) {
   regiss.value = value;
@@ -88,9 +88,32 @@ async function citalocal() {
       <div class="form-cita">
                    <label for="fechaCita">Fecha de la cita:</label>
                    <input type="datetime-local" v-model="fechaCita" @input="validarFecha" />
-                   <br>
                     <span>Motivo</span>
                     <textarea v-model="motivo"></textarea>
+           </div>
+
+           <div class="sinregis">
+            <span>Elige los productos para el servicio:</span>
+      <div class="search">
+    <input :type="inputType" class="search__input" v-model="search" placeholder="Buscar producto..." @input="productocadena">
+    <button class="search__button" @click="buscar">
+        <svg class="search__icon" aria-hidden="true" viewBox="0 0 24 24">
+            <g>
+                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+            </g>
+        </svg>
+    </button>
+    <br>
+         </div>
+        <div class="combo">
+          <div class="select" v-for="producto in productos" :key="producto.id">
+            <input class="he2" :disabled="!producto.puedeSeleccionar" type="checkbox" v-model="producto.seleccionado" @change="agregarAlArray(producto)" />
+            <span>{{ producto.nom_producto }}</span>
+            <input class="he1" type="number" v-model="producto.cantidad" placeholder="Cantidad" @input="checkCantidad(producto)" />
+        </div>
+      </div>
+
+
            </div>
        <div class="form2" v-if="regiss" >
                 <div class="form-cliente">
@@ -157,7 +180,7 @@ async function citalocal() {
       font-family: comfortaa;
       display: grid;
       grid-template-columns: 15% 70% 15%;
-      grid-template-rows: 3% 5% 10% 25% 5% 38% 5% 10% ;
+      grid-template-rows: 3% 5% 8% 25% 7% 38% 5% 10% ;
 }
 
 .tit{
@@ -182,8 +205,18 @@ async function citalocal() {
       grid-row-start: 4;
       grid-column-start: 2;
 }
+
+.sinregis{
+      display: flex;
+      height: 100%;
+      width: 100%;
+      align-items: center;
+      justify-content: center;
+      flex-direction: column;
+      grid-row-start: 6;
+      grid-column-start: 2;
+}
 .form2{
-      border-radius: 10px;
       display: flex;
       height: 100%;
       width: 100%;
@@ -246,4 +279,129 @@ textarea{
       width: 100%;
       resize: none;
 }
+
+.search-box{
+    font-family: inherit;
+  font-size: inherit;
+  box-shadow: 0 0 1em #00000013;
+  border: none;
+  color: #646464;
+  padding: 0.7rem 1rem;
+  border-radius: 30px;
+  width: 17em;
+  transition: all ease-in-out .5s;
+  margin-right: -2rem;
+  text-align: center;
+  }
+
+  .search-boxx{
+    font-family: inherit;
+  font-size: inherit;
+  box-shadow: 0 0 1em #00000013;
+  border: none;
+  color: #646464;
+  padding: 0.7rem 1rem;
+  border-radius: 30px;
+  width: 17em;
+  transition: all ease-in-out .5s;
+  margin-right: -2rem;
+  text-align: left;
+  height: 8vh;
+  }
+  
+
+  .select{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 0 2em #00000041;
+  border-radius: 20px;
+  height: 20vh;
+}
+
+.combo{
+  width: 100%;
+  height: 35vh;
+  overflow: scroll;
+  box-shadow: 0 0 2em #00000015;
+  display: flex;
+  flex-direction: column;
+  row-gap: 15%;
+  border-top: 20px;
+}
+
+.combo::-webkit-scrollbar {
+  width: 15px;
+}
+
+.combo::-webkit-scrollbar-track {
+  background-color: white;
+}
+
+.combo::-webkit-scrollbar-thumb {
+  background: #cfcfcf;
+}
+.search__input {
+  font-family: inherit;
+  font-size: inherit;
+  box-shadow: 0 0 1em #00000013;
+  border: none;
+  color: #646464;
+  padding: 0.7rem 1rem;
+  border-radius: 30px;
+  width: 100%;
+  transition: all ease-in-out .5s;
+  margin-right: -2rem;
+}
+.he2{
+  width: 3vh;
+  height: 3vh;
+  border: none;
+  box-shadow: 0 0 1em #00000013;
+border-radius: 10px;
+
+}
+.he1{
+  border: none;
+  border-radius: 30px;
+  height: 4vh;
+  width: 30vh;
+  box-shadow: 0 0 1em #00000013;
+  text-align: center;
+}
+.search__input:hover, .search__input:focus {
+  box-shadow: 0 0 1em #00000013;
+}
+
+.search__input:focus {
+  outline: none;
+  background-color: #f0eeee;
+}
+
+.search__input::-webkit-input-placeholder {
+  font-weight: 100;
+  color: #ccc;
+}
+
+.search__input:focus + .search__button {
+    background-color: transparent;
+}
+
+.search__button {
+  border: none;
+  background-color: transparent;
+  margin-top: .1em;
+}
+
+.search__button:hover {
+  cursor: pointer;
+}
+
+.search__icon {
+  height: 1.3em;
+  width: 1.3em;
+  fill: #b4b4b4;
+}
+
 </style>
